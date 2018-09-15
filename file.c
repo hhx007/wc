12274d04e455bfeb09ea906a52a11276ed7b588a
hhx007 <43274340+hhx007@users.noreply.github.com>
2018-09-15 12:32:29 +0800

    Fix bug

--- file.c
+++ file.c
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main(){
 printf("hello world!");
-=======
-#include <stdio.h>
-int main(){
-printf("hello world!");
->>>>>>> 3804bf33f07fad8168bf2cdc364cd08274905fcd
 }
